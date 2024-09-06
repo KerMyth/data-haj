6a72ec0532bba650839c9762ee7a299bf3e1f507
KerMyth <bimassha@gmail.com>
2024-09-06 16:28:55 +0700

    last

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/latihan-project-input/", // ganti dengan nama repository
+  plugins: [react()], // ganti dengan nama repository
 });
